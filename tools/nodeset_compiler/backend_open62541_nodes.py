86c6a0a758677089705f156216cc6d6c998b1553
matzy <matzy@debian>
2018-12-31 00:53:59 +0100

    enums

--- tools/nodeset_compiler/backend_open62541_nodes.py
+++ tools/nodeset_compiler/backend_open62541_nodes.py
@@ -248,6 +248,9 @@ def getTypeBrowseName(dataTypeNode):
         # in the stack we define a separate structure for the numeric range, but
         # the value itself is just a string
         typeBrowseName = "String"
+    elif typeBrowseName == "IdType":
+        # it's an enum, use Int32 for c++ compiler
+        typeBrowseName = "Int32"
     return typeBrowseName
 
 def generateValueCodeDummy(dataTypeNode, parentNode, nodeset):
