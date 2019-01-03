612fad7fb9060b2ed08746865e68e4602f45a588
matkonnerth <matkonnerth@gmail.com>
2019-01-03 12:27:12 +0100

    launch config, experimenting wit structure definition

--- examples/nodeset/server_testnodeset.c
+++ examples/nodeset/server_testnodeset.c
@@ -26,6 +26,8 @@ int main(int argc, char** argv) {
     UA_ServerConfig *config = UA_ServerConfig_new_default();
     UA_Server *server = UA_Server_new(config);
 
+    //config->
+
     UA_StatusCode retval;
     /* create nodes from nodeset */
     if( ua_namespace_testnodeset (server) != UA_STATUSCODE_GOOD) {
@@ -41,7 +43,22 @@ int main(int argc, char** argv) {
         printf("point 2d x: %d y: %d \n", p->x, p->y);       
 
         UA_Server_readValue(server, UA_NODEID_NUMERIC(2,6002), &out);            
-        printf("point 3d x: %f y: %f z: %f \n", ((UA_Point3D*)out.data)->x, ((UA_Point3D*)out.data)->y, ((UA_Point3D*)out.data)->z);        
+        printf("point 3d x: %f y: %f z: %f \n", ((UA_Point3D*)out.data)->x, ((UA_Point3D*)out.data)->y, ((UA_Point3D*)out.data)->z);
+
+        UA_StructureDefinition structdef;
+        structdef.defaultEncodingId = UA_NODEID_NUMERIC(2,3002);
+        structdef.baseDataType = UA_NODEID_NUMERIC(0,22);
+        structdef.structureType = UA_STRUCTURETYPE_STRUCTURE;
+        UA_StructureField fields[3];
+        fields[0].name = UA_STRING("x");
+        fields[0].description = UA_LOCALIZEDTEXT("de", "x");
+        fields[0].dataType = UA_NODEID_NUMERIC(0, 7);
+        fields[0].valueRank = -1;
+        fields[0].arrayDimensionsSize = 0;
+        fields[0].arrayDimensions = NULL;
+        fields[0].isOptional = false;
+        
+
 
         retval = UA_Server_run(server, &running);
     }
