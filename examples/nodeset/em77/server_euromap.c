fece4b711eff505206a895edbe7fea89ba7b1a70
matkonnerth <matkonnerth@gmail.com>
2019-05-10 20:12:07 +0200

    em77

---
+++ examples/nodeset/em77/server_euromap.c
@@ -0,0 +1,62 @@
+/* This work is licensed under a Creative Commons CCZero 1.0 Universal License.
+ * See http://creativecommons.org/publicdomain/zero/1.0/ for more information. */
+
+#ifdef UA_ENABLE_AMALGAMATION
+#include "open62541.h"
+#else
+#include "open62541/server.h"
+#include "open62541/plugin/log_stdout.h"
+#include "open62541/server_config_default.h"
+
+#endif
+
+#include <signal.h>
+#include <stdlib.h>
+
+#include "namespace_di_generated.h"
+#include "namespace_euromap83_generated.h"
+#include "namespace_euromap77_generated.h"
+
+UA_Boolean running = true;
+
+static void stopHandler(int sign) {
+    UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_SERVER, "received ctrl-c");
+    running = false;
+}
+
+int main(int argc, char** argv) {
+    signal(SIGINT, stopHandler);
+    signal(SIGTERM, stopHandler);
+
+    UA_ServerConfig *config = UA_ServerConfig_new_minimal(4842, NULL); // set the port// UA_ServerConfig_new_default();
+    UA_Server *server = UA_Server_new(config);
+
+
+    /* create nodes from nodeset */
+    UA_StatusCode retval = namespace_di_generated(server);
+    if(retval != UA_STATUSCODE_GOOD) {
+        UA_LOG_ERROR(UA_Log_Stdout, UA_LOGCATEGORY_SERVER, "Adding the DI namespace failed. Please check previous error output.");
+        UA_Server_delete(server);
+        UA_ServerConfig_delete(config);
+        return EXIT_FAILURE;
+    }
+    retval |= namespace_euromap83_generated(server);
+    if(retval != UA_STATUSCODE_GOOD) {
+        UA_LOG_ERROR(UA_Log_Stdout, UA_LOGCATEGORY_SERVER, "Adding the Euromap83 namespace failed. Please check previous error output.");
+        UA_Server_delete(server);
+        UA_ServerConfig_delete(config);
+        return EXIT_FAILURE;
+    }
+    retval |= namespace_euromap77_generated(server);
+    if(retval != UA_STATUSCODE_GOOD) {
+        UA_LOG_ERROR(UA_Log_Stdout, UA_LOGCATEGORY_SERVER, "Adding the Euromap77 namespace failed. Please check previous error output.");
+        UA_Server_delete(server);
+        UA_ServerConfig_delete(config);
+        return EXIT_FAILURE;
+    }
+
+    retval = UA_Server_run(server, &running);
+    UA_Server_delete(server);
+    UA_ServerConfig_delete(config);
+    return retval == UA_STATUSCODE_GOOD ? EXIT_SUCCESS : EXIT_FAILURE;
+}
