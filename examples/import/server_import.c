25da5d08c73a59e1579a8aca2340bfc038e8af4c
matkonnerth <matkonnerth@gmail.com>
2019-06-01 15:07:00 +0200

    add variabletype handling 	geändert:       examples/import/server_import.c

--- examples/import/server_import.c
+++ examples/import/server_import.c
@@ -296,18 +296,53 @@ void myCallback(const TNode* node)
             break;
         }
 
+        case NODECLASS_VARIABLETYPE:
+        {
+            UA_VariableTypeAttributes attr = UA_VariableTypeAttributes_default;
+            attr.valueRank = atoi(((const TVariableTypeNode *)node)->valueRank);
+            attr.isAbstract = ((const TVariableTypeNode *)node)->isAbstract;
+            if(attr.valueRank>=0)
+            {
+                if(!strcmp(((const TVariableTypeNode *)node)->arrayDimensions, ""))
+                {
+                    attr.arrayDimensionsSize = 1;
+                    UA_UInt32 arrayDimensions[1];
+                    arrayDimensions[0] = 0;
+                    attr.arrayDimensions = &arrayDimensions[0];
+                }
+            }
+
+
+            Reference *ref = getHierachicalInverseReference(node);            
+            UA_NodeId parentId = getReferenceTarget(ref);
+            UA_NodeId refId = getReferenceTypeId(ref);
+            UA_NodeId typeDefId = getTypeDefinitionIdFromChars2(node);
+
+            UA_StatusCode retval = UA_Server_addVariableTypeNode(
+                server, id, parentId, refId, UA_QUALIFIEDNAME(1, node->browseName), typeDefId, attr,
+                NULL, NULL);
+
+            if(retval != UA_STATUSCODE_GOOD) {
+                printf("adding variabletype node %s failed\n", node->id.idString);
+            }
+        } break;
+
         case NODECLASS_VARIABLE:
         {
             UA_VariableAttributes attr = UA_VariableAttributes_default;
             attr.dataType = getNodeIdFromChars(((const TVariableNode *)node)->datatype);
             attr.valueRank = atoi(((const TVariableNode *)node)->valueRank);
-            char *tmp = "";
-            if(strcmp(((const TVariableNode *)node)->arrayDimensions, tmp))
+            if(attr.valueRank>=0)
             {
-                attr.arrayDimensionsSize = 1;
-                UA_UInt32 dim = (UA_UInt32)atoi(((const TVariableNode *)node)->arrayDimensions);
-                attr.arrayDimensions = &dim;
+                if(!strcmp(((const TVariableNode *)node)->arrayDimensions, ""))
+                {
+                    attr.arrayDimensionsSize = 1;
+                    UA_UInt32 arrayDimensions[1];
+                    arrayDimensions[0] = 0;
+                    attr.arrayDimensions = &arrayDimensions[0];
+                }
             }
+            
 
             UA_NodeId parentId =
                 getNodeIdFromChars(((const TVariableNode *)node)->parentNodeId);
