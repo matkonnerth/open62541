1f7249e3d6d026fde380fd0e5aa6dc90a209011c
mkonnerth <matkonnerth@gmail.com>
2023-04-02 07:12:23 +0200

    add toolchain

---
+++ tools/cmake/Toolchain-wasm32-wasi.cmake
@@ -0,0 +1,31 @@
+# Toolchain for 32-bit WebAssembly with WASI based libc
+#
+# https://github.com/WebAssembly/wasi-sdk
+#
+# The WASI sysroot must be provided in the environment variable WASI_SYSROOT.
+
+set(CMAKE_SYSTEM_NAME Generic)
+
+set(CMAKE_SYSTEM_PROCESSOR wasm32)
+set(triple ${CMAKE_SYSTEM_PROCESSOR}-wasi)
+
+#set(CMAKE_SYSROOT $ENV{WASI_SYSROOT})
+
+set(CMAKE_EXECUTABLE_SUFFIX_C ".html")
+set(CMAKE_C_COMPILER emcc)
+set(CMAKE_C_COMPILER_TARGET ${triple})
+set(CMAKE_C_FLAGS_INIT "-D__wasi__")
+
+set(CMAKE_EXECUTABLE_SUFFIX_CXX ".html")
+set(CMAKE_CXX_COMPILER emcc)
+set(CMAKE_CXX_COMPILER_TARGET ${triple})
+set(CMAKE_CXX_FLAGS_INIT "-D__wasi__")
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+
+#set(CMAKE_EXECUTABLE_SUFFIX ".html")
+
+SET(CMAKE_EXE_LINKER_FLAGS  "${CMAKE_EXE_LINKER_FLAGS} -lwebsocket.js -sPROXY_POSIX_SOCKETS -sUSE_PTHREADS -sPROXY_TO_PTHREAD")
